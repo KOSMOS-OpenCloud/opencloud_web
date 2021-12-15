amrita-shrestha/web

add test download previos version

tests/acceptance/features/webUIFilesActionMenu/versions.feature
@@ -87,12 +87,24 @@ Feature: Versions of a file
   @issue-ocis-2319
   Scenario: change the file content of a received shared file
     Given the setting "shareapi_auto_accept_share" of app "core" has been set to "no"
-    And the administrator has set the default folder for received shares to "Shares"
-    And user "user0" has uploaded file with content "a text file" to "lorem.txt"
-    And user "user0" has shared file "lorem.txt" with user "Alice" with "all" permissions
-    And user "Alice" has accepted the share "Shares/lorem.txt" offered by user "user0"
+    And user "Brian" has been created with default attributes and without skeleton files
+    And user "Brian" has created folder "simple-folder"
+    And user "Brian" has created file "simple-folder/lorem.txt"
+    And user "Brian" has shared folder "simple-folder" with user "Alice" with "all" permissions
+    And user "Alice" has accepted the share "simple-folder" offered by user "Brian"
     And user "Alice" has logged in using the webUI
-    When the user opens folder "Shares" using the webUI
-    And the user uploads overwriting file "lorem.txt" using the webUI
+    And the user has opened folder "Shares"
+    And the user has opened folder "simple-folder"
+    When the user uploads overwriting file "lorem.txt" using the webUI
     And the user browses to display the "versions" details of file "lorem.txt"
-    Then the versions list should contain 1 entries
+    Then the versions list should contain 1 entries 
+
+
+  @issue-ocis-2261
+  Scenario: user downloads a previous version of the file
+    Given user "Alice" has uploaded file with content "lorem" to "lorem.txt"
+    And user "Alice" has uploaded file with content "lorem content" to "lorem.txt"
+    And user "Alice" has logged in using the webUI
+    And the user browses to display the "versions" details of file "lorem.txt"
+    When the user downloads a file of a previous version using the webUI
+    Then as "Alice" the content of "lorem.txt" should be the same as the content of local file "lorem.txt"    

tests/acceptance/stepDefinitions/filesContext.js
@@ -682,6 +682,10 @@ Then(
   }
 )
 
+When('the user downloads a file of a previous version using the webUI', function() {
+  return client.page.FilesPageElement.versionsDialog().downloadFilePreviousVersion()
+})
+
 /**
  *
  * @param {DataTable} entryList the list needs a heading line

KOSMOS-OpenCloud/opencloud_web

tests/acceptance/features/webUIFilesActionMenu/versions.feature
@@ -77,18 +77,23 @@ Feature: Versions of a file
   Scenario: change the file content of a received shared file
     Given the setting "shareapi_auto_accept_share" of app "core" has been set to "no"
     And the administrator has set the default folder for received shares to "Shares"
-    And user "user0" has uploaded file with content "a text file" to "lorem.txt"
-    And user "user0" has shared file "lorem.txt" with user "Alice" with "all" permissions
-    And user "Alice" has accepted the share "Shares/lorem.txt" offered by user "user0"
+    And user "Brian" has been created with default attributes and without skeleton files
+    And user "Brian" has created folder "simple-folder"
+    And user "Brian" has uploaded file with content "a text file" to "simple-folder/lorem.txt"
+    And user "Brian" has shared folder "simple-folder" with user "Alice" with "all" permissions
+    And user "Alice" has accepted the share "simple-folder" offered by user "Brian"
     And user "Alice" has logged in using the webUI
-    When the user opens folder "Shares" using the webUI
-    And the user uploads overwriting file "lorem.txt" using the webUI
+    And the user has opened folder "Shares"
+    And the user has opened folder "simple-folder"
+    When the user uploads overwriting file "lorem.txt" using the webUI
     And the user browses to display the "versions" details of file "lorem.txt"
-    Then the versions list should contain 1 entries
+    Then the versions list should contain 1 entries 
 
- @issue-ocis-1328 @disablePreviews
+
+  @issue-ocis-1328 @disablePreviews
   Scenario: sharee can see the versions of a file
-    Given user "user0" has uploaded file with content "lorem content" to "lorem-file.txt"
+    Given the setting "shareapi_auto_accept_share" of app "core" has been set to "yes"
+    And user "user0" has uploaded file with content "lorem content" to "lorem-file.txt"
     And user "user0" has uploaded file with content "lorem" to "lorem-file.txt"
     And user "user0" has uploaded file with content "new lorem content" to "lorem-file.txt"
     And user "user0" has shared file "lorem-file.txt" with user "Alice"
@@ -97,3 +102,12 @@ Feature: Versions of a file
     Then the content of file "lorem-file.txt" for user "Alice" should be "new lorem content"
     And the versions list should contain 2 entries
 
+
+  @issue-ocis-2261
+  Scenario: user downloads a previous version of the file
+    Given user "Alice" has uploaded file with content "lorem" to "lorem.txt"
+    And user "Alice" has uploaded file with content "lorem content" to "lorem.txt"
+    And user "Alice" has logged in using the webUI
+    And the user browses to display the "versions" details of file "lorem.txt"
+    When the user downloads a previous version of the file using the webUI
+    Then as "Alice" the content of "lorem.txt" should be the same as the content of local file "lorem.txt"    

tests/acceptance/stepDefinitions/filesContext.js
@@ -697,6 +697,10 @@ Then(
   }
 )
 
+When('the user downloads a previous version of the file using the webUI', function () {
+  return client.page.FilesPageElement.versionsDialog().downloadFilePreviousVersion()
+})
+
 /**
  *
  * @param {DataTable} entryList the list needs a heading line